a87171b7d56f496fc2fdb727dc5f9d2d31351bbe
marcodedemo <marco.dedemo@gmail.com>
2023-04-11 18:52:45 +0200

    feat: fixed items on page

--- src/App.vue
+++ src/App.vue
@@ -6,12 +6,14 @@
   import AppFooter from "./components/AppFooter.vue";
 
   export default{
+
     data(){
       return{
 
 
 
       }
+      
     },
 
     components:{
@@ -19,7 +21,17 @@
       AppMain,
       AppFooter,
 
-    }
+    },
+
+    methods:{
+      scrollToTop(){
+        window.scrollTo({
+          top:0,
+        })
+      }
+    },
+
+    
   }
 
 </script>
@@ -28,12 +40,16 @@
 /* -------------------------------- template -------------------------------- */
 <template>
 
-  <div>
+  <div id="page">
 
     <AppHeader></AppHeader>
     <AppMain></AppMain>
     <AppFooter></AppFooter>
 
+    <div id="fixed-items">
+      <div id="top" @click="scrollToTop()"><i class="fa-solid fa-arrow-up"></i></div>
+      <div id="message"><i class="fa-regular fa-comment"></i></div>
+    </div>
 
   </div>
 
@@ -43,6 +59,48 @@
 /* ---------------------------------- style --------------------------------- */
 <style lang="scss" scoped>
 
+#page{
+  position: relative;
+}
+#fixed-items{
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 20px;
 
+  position: fixed;
+  bottom: 20px;
+  right: 20px;
 
+  #top{
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    
+    width: 30px;
+    height: 30px;
+    
+    background-color: white;
+    color: black;
+    border: 1px solid black;
+    padding: 10px;
+    border-radius: 50%;
+  }
+  
+  #message{
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    
+    width: 60px;
+    height: 60px;
+    
+    background-color: #517ceb;
+    color: white;
+    font-size: 1.6em;
+    padding: 10px;
+    border-radius: 50%;
+  }
+}
+  
 </style>
